37e83a085b84169106f56048d516cd88da91392e
Kyle Brown <kyleb@solutionreach.com>
2018-02-19 20:09:43 -0700

    Updated entrypoint to match app.json env var names

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,9 +6,9 @@ if [ "${1:0:1}" = '-' ] || [ "$1" = "$main_cmd" ]; then
   if [ "$1" = "$main_cmd" ]; then
     shift
   fi
-  [ "$username" ] && set -- --username "$username" "$@"
-  [ "$password" ] && set -- --password "$password" "$@"
-  [ "$forward" ] && set -- --forward "$forward" "$@"
+  [ "$GMAIL_USER" ] && set -- --username "$GMAIL_USER" "$@"
+  [ "$GMAIL_APP_PASSWORD" ] && set -- --password "$GMAIL_APP_PASSWORD" "$@"
+  [ "$FORWARD_ADDRESS" ] && set -- --forward "$FORWARD_ADDRESS" "$@"
 	set -- $main_cmd "$@"
 fi
 
